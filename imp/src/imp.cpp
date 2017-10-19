87e83bdb4f1bb2a626eb0b319e8da30b8a6c73e2
Björn Ritzl <bjorn.ritzl@gmail.com>
2017-10-19 12:39:06 +0200

    Multiple improvements

--- imp/src/imp.cpp
+++ imp/src/imp.cpp
@@ -6,6 +6,17 @@
 
 #include <dmsdk/sdk.h>
 
+
+struct Kernel {
+    double values[9];
+};
+
+struct BufferData {
+    dmhash_t name;
+    dmBuffer::ValueType type;
+    uint32_t count;
+};
+
 static void printStack(lua_State* L) {
     int n = lua_gettop(L);
     for (int i = 1; i <= n; i++)  {
@@ -16,8 +27,7 @@ static void printStack(lua_State* L) {
 
 
 static double double_from_table(lua_State* L, int table_index, int table_key) {
-    lua_pushnumber(L, table_key);
-    lua_gettable(L, table_index);
+    lua_rawgeti(L, table_index, table_key);
     double n = luaL_checknumber(L, lua_gettop(L));
     lua_pop(L, 1);
     return n;
@@ -33,47 +43,78 @@ static int xytoi(int x, int y, int w, int h, int bpp) {
 }
 
 
-
-static int apply_filter(int x, int y, int w, int h, int bpp, int offset, double* filter, uint8_t* pixels) {
+static int apply_kernel(int x, int y, int w, int h, int bpp, int offset, Kernel kernel, uint8_t* pixels) {
     double sum = 0;
-    sum += pixels[xytoi(x - 1, y - 1, w, h, bpp) + offset] * filter[0];
-    sum += pixels[xytoi(x - 0, y - 1, w, h, bpp) + offset] * filter[1];
-    sum += pixels[xytoi(x + 1, y - 1, w, h, bpp) + offset] * filter[2];
-    sum += pixels[xytoi(x - 1, y - 0, w, h, bpp) + offset] * filter[3];
-    sum += pixels[xytoi(x - 0, y - 0, w, h, bpp) + offset] * filter[4];
-    sum += pixels[xytoi(x + 1, y - 0, w, h, bpp) + offset] * filter[5];
-    sum += pixels[xytoi(x - 1, y + 1, w, h, bpp) + offset] * filter[6];
-    sum += pixels[xytoi(x - 0, y + 1, w, h, bpp) + offset] * filter[7];
-    sum += pixels[xytoi(x + 1, y + 1, w, h, bpp) + offset] * filter[8];
+    sum += pixels[xytoi(x - 1, y - 1, w, h, bpp) + offset] * kernel.values[0];
+    sum += pixels[xytoi(x - 0, y - 1, w, h, bpp) + offset] * kernel.values[1];
+    sum += pixels[xytoi(x + 1, y - 1, w, h, bpp) + offset] * kernel.values[2];
+    sum += pixels[xytoi(x - 1, y - 0, w, h, bpp) + offset] * kernel.values[3];
+    sum += pixels[xytoi(x - 0, y - 0, w, h, bpp) + offset] * kernel.values[4];
+    sum += pixels[xytoi(x + 1, y - 0, w, h, bpp) + offset] * kernel.values[5];
+    sum += pixels[xytoi(x - 1, y + 1, w, h, bpp) + offset] * kernel.values[6];
+    sum += pixels[xytoi(x - 0, y + 1, w, h, bpp) + offset] * kernel.values[7];
+    sum += pixels[xytoi(x + 1, y + 1, w, h, bpp) + offset] * kernel.values[8];
     if (sum < 0) sum = 0;
     if (sum > 255) sum = 255;
     return sum;
 }
 
+static Kernel check_kernel(lua_State* L, int index) {
+    luaL_checktype(L, 5, LUA_TTABLE);
+    Kernel kernel;
+    kernel.values[0] = double_from_table(L, 5, 1);
+    kernel.values[1] = double_from_table(L, 5, 2);
+    kernel.values[2] = double_from_table(L, 5, 3);
+    kernel.values[3] = double_from_table(L, 5, 4);
+    kernel.values[4] = double_from_table(L, 5, 5);
+    kernel.values[5] = double_from_table(L, 5, 6);
+    kernel.values[6] = double_from_table(L, 5, 7);
+    kernel.values[7] = double_from_table(L, 5, 8);
+    kernel.values[8] = double_from_table(L, 5, 9);
+    return kernel;
+}
+
+static BufferData get_buffer_data(dmBuffer::HBuffer buffer) {
+    BufferData data;
+    dmBuffer::GetStreamName(buffer, 0, &data.name);
+    dmBuffer::GetStreamType(buffer, data.name, &data.type, &data.count);
+    return data;
+}
+
+
+static dmBuffer::HBuffer check_and_validate_buffer(lua_State* L, int index) {
+    dmScript::LuaHBuffer *lua_buffer = dmScript::CheckBuffer(L, index);
+    dmBuffer::HBuffer buffer = lua_buffer->m_Buffer;
+
+    if (!dmBuffer::IsBufferValid(buffer)) {
+        luaL_error(L, "Buffer is invalid");
+    }
+    BufferData data = get_buffer_data(buffer);
+    if (data.type != dmBuffer::VALUE_TYPE_UINT8) {
+        luaL_error(L, "Buffer is not of type UINT8");
+    }
+    return buffer;
+}
+
+static bool compare_buffers(dmBuffer::HBuffer a, dmBuffer::HBuffer b) {
+    BufferData a_data = get_buffer_data(a);
+    BufferData b_data = get_buffer_data(b);
+    return (a_data.count == b_data.count);
+}
 
 static int Apply(lua_State* L) {
     int top = lua_gettop(L);
 
-    dmScript::LuaHBuffer *lua_src_buffer = dmScript::CheckBuffer(L, 1);
-    uint32_t width = luaL_checknumber(L, 2);
-    uint32_t height = luaL_checknumber(L, 3);
-    luaL_checktype(L, 4, LUA_TTABLE);
+    dmBuffer::HBuffer src_buffer = check_and_validate_buffer(L, 1);
+    dmBuffer::HBuffer dst_buffer = check_and_validate_buffer(L, 2);
+    uint32_t width = luaL_checknumber(L, 3);
+    uint32_t height = luaL_checknumber(L, 4);
+    Kernel kernel = check_kernel(L, 5);
 
-    double filter[9];
-    filter[0] = double_from_table(L, 4, 1);
-    filter[1] = double_from_table(L, 4, 2);
-    filter[2] = double_from_table(L, 4, 3);
-    filter[3] = double_from_table(L, 4, 4);
-    filter[4] = double_from_table(L, 4, 5);
-    filter[5] = double_from_table(L, 4, 6);
-    filter[6] = double_from_table(L, 4, 7);
-    filter[7] = double_from_table(L, 4, 8);
-    filter[8] = double_from_table(L, 4, 9);
-    // for(int i=0; i < 9; i++) {
-    //     dmLogError("filter at %d is %f", i, filter[i]);
-    // }
+    if(!compare_buffers(src_buffer, dst_buffer)) {
+        luaL_error(L, "Buffers are not of the same length");
+    }
 
-    dmBuffer::HBuffer src_buffer = lua_src_buffer->m_Buffer;
     dmBuffer::Result r;
 
     // get stream name, type and count
@@ -95,7 +136,6 @@ static int Apply(lua_State* L) {
         return 0;
     }
 
-
     // get source buffer bytes
     uint8_t* src_bytes = 0;
     uint32_t src_size = 0;
@@ -104,20 +144,6 @@ static int Apply(lua_State* L) {
         dmLogError("Unable to get bytes from source buffer");
         return 0;
     }
-
-
-    // create destination buffer
-    dmBuffer::HBuffer dst_buffer;
-    dmBuffer::StreamDeclaration dstStreamsDecl[] = {
-    {
-        stream_name, dmBuffer::VALUE_TYPE_UINT8, (uint8_t)stream_type_count }
-    };
-    r = dmBuffer::Create(src_size / stream_type_count, dstStreamsDecl, 1, &dst_buffer);
-    if (r != dmBuffer::RESULT_OK) {
-        dmLogError("Unable to create target buffer");
-        return 0;
-    }
-
 
     // get destination buffer bytes
     uint8_t* dst_bytes = 0;
@@ -128,26 +154,21 @@ static int Apply(lua_State* L) {
         return 0;
     }
 
-
-    // apply filter
+    // apply kernel
     for(int x = 0; x < width; x++) {
         for(int y = 0; y < height; y++) {
             int i = xytoi(x, y, width, height, stream_type_count);
-            dst_bytes[i + 0] = apply_filter(x, y, width, height, stream_type_count, 0, filter, src_bytes);
-            dst_bytes[i + 1] = apply_filter(x, y, width, height, stream_type_count, 1, filter, src_bytes);
-            dst_bytes[i + 2] = apply_filter(x, y, width, height, stream_type_count, 2, filter, src_bytes);
+            dst_bytes[i + 0] = apply_kernel(x, y, width, height, stream_type_count, 0, kernel, src_bytes);
+            dst_bytes[i + 1] = apply_kernel(x, y, width, height, stream_type_count, 1, kernel, src_bytes);
+            dst_bytes[i + 2] = apply_kernel(x, y, width, height, stream_type_count, 2, kernel, src_bytes);
             if(stream_type_count == 4) {
-                dst_bytes[i + 3] = apply_filter(x, y, width, height, stream_type_count, 3, filter, src_bytes);
+                dst_bytes[i + 3] = apply_kernel(x, y, width, height, stream_type_count, 3, kernel, src_bytes);
             }
         }
     }
 
-    // return destination buffer
-    dmScript::LuaHBuffer lua_dst_buffer = { dst_buffer, true };
-    dmScript::PushBuffer(L, lua_dst_buffer);
-
-    assert(top + 1 == lua_gettop(L));
-    return 1;
+    assert(top == lua_gettop(L));
+    return 0;
 }
 
 
